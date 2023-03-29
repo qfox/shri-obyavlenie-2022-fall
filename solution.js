061464711308e6654e28fd40eb09f95191723dab
Alexey Yaroshevich <qfox@yandex-team.ru>
2023-03-29 07:41:21 +0300

    step3

--- solution.js
+++ solution.js
@@ -1,3 +1,6 @@
-function renderWaterfall(rootNode, columnCount, elementGap) {
+/** @typedef {import('./solution').RenderWaterfall} RenderWaterfall */
+
+/** @type {RenderWaterfall} */
+function renderWaterfall(rootNode, columnCount = 3, gap = 20) {
   console.log(rootNode);
 }
---
+++ solution.ts
@@ -0,0 +1,3 @@
+export interface RenderWaterfall {
+  (rootNode: HTMLElement, columnCount: number, gap: number): void;
+}
