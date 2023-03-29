afd67e5f5c2ed54ae461431e03197d2c1aae81a2
Alexey Yaroshevich <qfox@yandex-team.ru>
2023-03-29 08:32:23 +0300

    step4

--- solution.js
+++ solution.js
@@ -2,5 +2,19 @@
 
 /** @type {RenderWaterfall} */
 function renderWaterfall(rootNode, columnCount = 3, gap = 20) {
-  console.log(rootNode);
+    rootNode.style.display = 'flex';
+    rootNode.style.justifyContent = 'space-between';
+
+    const styleTag = document.createElement('style');
+    const css = `
+        .column :not(:last-child) {
+            margin-bottom: ${gap}px;
+        }
+    `;
+
+    styleTag.appendChild(document.createTextNode(css));
+    document.body.appendChild(styleTag);
+
+    const allGapsWidth = gap * (columnCount - 1);
+    const columnWidth = (rootNode.offsetWidth - allGapsWidth) / columnCount;
 }
